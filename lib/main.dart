026fe31e156034c5c307128a0c556ac6356d409e
mura4906 <gkpj4906@yahoo.co.jp>
2025-02-01 00:37:47 +0900

    firebase初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_web_app/firebase_firestore.dart';
+import 'package:my_web_app/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -31,7 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyFirestorePage(),
     );
   }
 }
